27f7072f5e9e009259d17180a01b86c24f076b07
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-22 22:25:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Jamil"
+  end
+  
+  get '/hometown' do
+    "My hometown is Queens"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Radioactive"
+  end
+  
 end
